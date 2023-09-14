6d40c39beaa5b8e506c6f5ea83083d444452b9f7
Mohammed Suhail <jrskmohammed@gmail.com>
2023-09-14 08:59:54 +0530

    Add files via upload

---
+++ myproject/src/test/java/com/docker/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
